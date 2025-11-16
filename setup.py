36d4adc4ff759235c7dcde3ec75f4a51d06a42e2
TobiKuehn7 <tobias1.kuehn@web.de>
2025-11-16 16:37:55 +0100

    adjusted for publishing v0.0.1 to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="test-fhir-client",
+    name="fhir-client",
     version="0.0.1",
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
