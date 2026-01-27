0f8f3100906c82f0b9520e651bab30b6d5bb1e0a
TobiKuehn7 <tobias1.kuehn@web.de>
2026-01-27 19:08:30 +0100

    updated version in setup py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="fhir-server-client",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
-    description="A Python client for FHIR servers",
+    description="A Python client to interact with FHIR servers",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/TobiKuehn7/fhir-client",
